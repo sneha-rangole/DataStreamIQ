c6987cea9b4bff312301f65e5e91810dce9e0476
Sneha Rangole <rangole.sneha10@gmail.com>
2024-11-13 02:35:01 -0500

    Added text file and updated functions

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, DateType
-from pyspark.sql.functions import udf,regexp_replace
+from pyspark.sql.functions import udf, regexp_replace
 from config.config import configuration
 from udf_utils import *
 
 # Function to define all user-defined functions (UDFs) for data processing.
 def define_udfs():
-    # UDFs are functions we define to apply custom transformations on data. 
-    # Here, each UDF transforms specific fields, such as extracting details from raw data, using custom logic.
     return {
         'extract_file_name_udf': udf(extract_file_name, StringType()),
         'extract_position_udf': udf(extract_position, StringType()),
-         # Extracts salary range from text into start and end fields within a structured format
         'extract_salary_udf': udf(extract_salary, StructType([
-            StructField('salary_start', DoubleType(),True),
-            StructField('salary_end', DoubleType(),True),
+            StructField('salary_start', DoubleType(), True),
+            StructField('salary_end', DoubleType(), True),
         ])),
-        # Other UDFs are defined similarly for various data fields (e.g., start/end dates, requirements)
-        # This helps maintain code modularity, reusability, and easier testing.
         'extract_start_date_udf': udf(extract_start_date, DateType()),
         'extract_end_date_udf': udf(extract_end_date, DateType()),
         'extract_classcode_udf': udf(extract_classcode, StringType()),
@@ -36,45 +31,25 @@ def define_udfs():
 # The following block of code ensures that the script is only executed if it is run directly (not imported as a module).
 if __name__ == "__main__":
 
-    # Creating a SparkSession. The SparkSession is the entry point to use Spark functionality.
-    # It's necessary to configure this session to interact with AWS S3 for storage.
     spark = (
-        # Build a SparkSession using the builder pattern
         SparkSession.builder
-            # Assign a name to the Spark application (this will appear in Spark's UI)
             .appName('DataStreamIQ')
-            
-            # Configure Spark to include the necessary AWS dependencies (Hadoop AWS & AWS Java SDK)
             .config('spark.jars.packages',
-                    'org.apache.hadoop:hadoop-aws:3.3.1,'  # Hadoop AWS package for S3 integration
-                    'com.amazonaws:aws-java-sdk:1.11.469')  # AWS SDK for Java for S3 access
-
-            # Configure Spark to use the S3A protocol for accessing Amazon S3
-            .config('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')  # S3A file system implementation
-            
-            # Provide the AWS access key and secret key, which are used for authenticating with AWS S3.
-            # These values are retrieved from a configuration (e.g., environment variables or a config file).
-            .config('spark.hadoop.fs.s3a.access.key', configuration.get('AWS_ACCESS_KEY'))  # Fetch AWS Access Key
-            .config('spark.hadoop.fs.s3a.secret.key', configuration.get('AWS_SECRET_KEY'))  # Fetch AWS Secret Key
-
-            # Specify the AWS credentials provider, which is responsible for supplying the access keys to Spark
+                    'org.apache.hadoop:hadoop-aws:3.3.1,'
+                    'com.amazonaws:aws-java-sdk:1.11.469')
+            .config('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
+            .config('spark.hadoop.fs.s3a.access.key', configuration.get('AWS_ACCESS_KEY'))
+            .config('spark.hadoop.fs.s3a.secret.key', configuration.get('AWS_SECRET_KEY'))
             .config('spark.hadoop.fs.s3a.aws.credentials.provider', 
-                    'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')  # Using simple credentials provider for AWS
-
-            # Get the configured SparkSession or create one if it doesn't already exist
-            .getOrCreate()  # SparkSession is initialized here
+                    'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
+            .getOrCreate()
     )
 
-    # Define paths to different input directories (for text, JSON, CSV, PDF, video, and image data)
-    text_input_dir = 'file:///Users/sneha_rangole/Desktop/GitCode/AWS_Big_Data_Project/input/input_text'
+    # Input directories for various types of data
     json_input_dir = 'file:///Users/sneha_rangole/Desktop/GitCode/AWS_Big_Data_Project/input/input_json'
-    csv_input_dir = 'file:///Users/sneha_rangole/Desktop/GitCode/AWS_Big_Data_Project/input/input_csv'
-    pdf_input_dir = 'file:///Users/sneha_rangole/Desktop/GitCode/AWS_Big_Data_Project/input/input_pdf'
-    video_input_dir = 'file:///Users/sneha_rangole/Desktop/GitCode/AWS_Big_Data_Project/input/input_video'
-    img_input_dir = 'file:///Users/sneha_rangole/Desktop/GitCode/AWS_Big_Data_Project/input/input_img'
-   
-    # Define a schema for the input data, specifying data types and structure of each column.
-    # This schema is necessary for structured processing in Spark DataFrames.
+    text_input_dir = 'file:///Users/sneha_rangole/Desktop/GitCode/AWS_Big_Data_Project/input/input_text'
+    
+    # Define schema for incoming data
     data_schema = StructType([
         StructField('file_name', StringType(), True),
         StructField('position', StringType(), True),
@@ -94,20 +69,23 @@ if __name__ == "__main__":
         StructField('application_location', StringType(), True),
     ])
 
-    # Call to define_udfs() to initialize all custom UDFs for data processing tasks.
+    # Define UDFs
     udfs = define_udfs()
 
-    # reading data from input directory   
-    # Define streaming DataFrame reading from the text input directory
+    # Read JSON data with multiline option
+    json_df = spark.readStream \
+        .option("multiline", "true") \
+        .json(json_input_dir, schema=data_schema)
+
+    # Read text data and process it with UDFs
     job_bulletine_df = (spark.readStream
-                    .format('text')
-                    .option('wholetext', 'true')
-                    .load(text_input_dir)
-                )
-    #Each step uses Spark DataFrame transformations and custom UDFs to parse, clean, and format the unstructured text data into a structured format,
-    # making it more suitable for analysis and reporting.
-    job_bulletins_df = job_bulletine_df.withColumn('file_name', regexp_replace(udfs['extract_file_name_udf']('value'),'\r',' ')) 
-    job_bulletins_df = job_bulletins_df.withColumn('value', regexp_replace('value', r'\n', ' '))
+                        .format('text')
+                        .option('wholetext', 'true')
+                        .load(text_input_dir)
+                    )
+
+    # Transform `job_bulletine_df` to have the same columns as `json_df`
+    job_bulletins_df = job_bulletine_df.withColumn('file_name', regexp_replace(udfs['extract_file_name_udf']('value'),r'\r',' '))
     job_bulletins_df = job_bulletins_df.withColumn('position', regexp_replace(udfs['extract_position_udf']('value'), r'\r',' '))
     job_bulletins_df = job_bulletins_df.withColumn('salary_start', udfs['extract_salary_udf']('value').getField('salary_start'))
     job_bulletins_df = job_bulletins_df.withColumn('salary_end', udfs['extract_salary_udf']('value').getField('salary_end'))
@@ -118,28 +96,35 @@ if __name__ == "__main__":
     job_bulletins_df = job_bulletins_df.withColumn('notes', udfs['extract_notes_udf']('value'))
     job_bulletins_df = job_bulletins_df.withColumn('duties', udfs['extract_duties_udf']('value'))
     job_bulletins_df = job_bulletins_df.withColumn('selection', udfs['extract_selection_udf']('value'))
-    job_bulletins_df = job_bulletins_df.withColumn('education_length', udfs['extract_education_length_udf']('value'))
-    job_bulletins_df = job_bulletins_df.withColumn('school_type', udfs['extract_school_type_udf']('value'))
     job_bulletins_df = job_bulletins_df.withColumn('experience_length', udfs['extract_experience_length_udf']('value'))
     job_bulletins_df = job_bulletins_df.withColumn('job_type', udfs['extract_job_type_udf']('value'))
+    job_bulletins_df = job_bulletins_df.withColumn('education_length', udfs['extract_education_length_udf']('value'))
+    job_bulletins_df = job_bulletins_df.withColumn('school_type', udfs['extract_school_type_udf']('value'))
     job_bulletins_df = job_bulletins_df.withColumn('application_location', udfs['extract_application_location_udf']('value'))
-    
-    j_df = job_bulletins_df.select(
-    'file_name', 'salary_start', 'salary_end', 'start_date', 'end_date', 
-    'req', 'classcode', 'notes', 'duties', 'selection', 'education_length', 
-    'school_type', 'experience_length', 'job_type', 'application_location'
+
+    # Now select the same columns as `json_df`
+    job_bulletins_df = job_bulletins_df.select(
+        'file_name', 'salary_start', 'salary_end', 'start_date', 'end_date', 
+        'req', 'classcode', 'notes', 'duties', 'selection', 'experience_length', 
+        'job_type', 'education_length', 'school_type', 'application_location'
     )
 
-    # json_df = json_df.select('file_name','position','salary','start_date','end_date','req','notes','duties','selection','education_length','school_type','experience_length','job_type','application_location')
+    json_df = json_df.select(
+        'file_name', 'salary_start', 'salary_end', 'start_date', 'end_date', 
+        'req', 'classcode', 'notes', 'duties', 'selection', 'experience_length', 
+        'job_type', 'education_length', 'school_type', 'application_location'
+    )
+    # Now you can union the DataFrames
+    union_dataframe = job_bulletins_df.union(json_df)
 
-    # Write the streaming DataFrame to the console for display
-    query = (j_df
+    # Write the resulting DataFrame to the console
+    query = (union_dataframe
              .writeStream
-             .outputMode("append")  # Use 'append' to show new data as it arrives
-             .format("console")       # Output the results to the console
+             .outputMode("append")
+             .format("console")
              .option('truncate', False)
-             .start()                # Start the streaming query
+             .start()
         )
     
-    # Wait for the termination of the stream to keep it active and allow viewing
+    # Wait for termination
     query.awaitTermination()
--- udf_utils.py
+++ udf_utils.py
@@ -2,18 +2,15 @@ import re
 from datetime import datetime
 
 def extract_file_name(file_content):
-    file_content = file_content.strip()
-    position = file_content.split('\n')[0]
-    return position
+    return file_content.strip().split('\n')[0]
 
 def extract_position(file_content):
-    return extract_file_name(file_content) 
+    return file_content.strip().split('\n')[0]
 
 def extract_classcode(file_content):
     try:
         classcode_match = re.search(r'(Class Code: )\s+(\d+)', file_content)
-        classcode = classcode_match.group(2) if classcode_match else None
-        return classcode
+        return classcode_match.group(2) if classcode_match else None
     except Exception as e:
         raise ValueError(f'Error extracting classcode: {e}')
 
@@ -24,9 +21,8 @@ def extract_salary(file_content):
         if salary_match:
             salary_start = float(salary_match.group(1).replace(',', ''))
             salary_end = float(salary_match.group(3).replace(',', '')) if salary_match.group(3) else float(salary_match.group(2).replace(',', ''))
-        else:
-            salary_start, salary_end = None, None
-        return salary_start, salary_end
+            return salary_start, salary_end
+        return None, None
     except Exception as e:
         raise ValueError(f'Error extracting salary: {str(e)}')
 
@@ -34,7 +30,7 @@ def extract_requirements(file_content):
     try:
         req_match = re.search(r'(REQUIREMENTS/MINIMUM QUALIFICATIONS)\s+(.+?)\s+(NOTES)', file_content, re.DOTALL)
         req = req_match.group(2) if req_match else None
-        return req.split("\r") if req else []  # Handle None and split safely
+        return req.splitlines() if req else []
     except Exception as e:
         raise ValueError(f'Error extracting requirements: {e}')
 
@@ -42,41 +38,40 @@ def extract_notes(file_content):
     try:
         notes_match = re.search(r'(NOTES)\s+(.+?)\s+(DUTIES)', file_content, re.DOTALL)
         notes = notes_match.group(2) if notes_match else None
-        return notes.split("\r") if notes else []  # Handle None and split safely
+        return notes.splitlines() if notes else []
     except Exception as e:
         raise ValueError(f'Error extracting notes: {e}')
 
 def extract_duties(file_content):
     try:
-        duties_match = re.search(r'(DUTIES):(.*?)(REQ[A-Z])', file_content, re.DOTALL)
-        duties = duties_match.group(2).strip() if duties_match else None
-        return duties
+        duties_match = re.search(r'(DUTIES)\s+(.+?)\s+(REQ)', file_content, re.DOTALL)
+        duties = duties_match.group(2) if duties_match else None
+        return duties.splitlines() if duties else []
     except Exception as e:
-        raise ValueError(f'Error extracting duties: {str(e)}')
+        raise ValueError(f'Error extracting duties: {e}')
 
 def extract_start_date(file_content):
     try:
-        opendate_match = re.search(r'(Open [Dd]ate:)\s+(\d\d-\d\d-\d\d)', file_content)
-        start_date = datetime.strptime(opendate_match.group(2), '%m-%d-%y') if opendate_match else None
-        return start_date
+        opendate_match = re.search(r'(Open Date: )\s+(\d{2}-\d{2}-\d{2})', file_content)
+        opendate = opendate_match.group(2) if opendate_match else None
+        return datetime.strptime(opendate, '%m-%d-%y') if opendate else None
     except Exception as e:
         raise ValueError(f'Error extracting opendate: {e}')
 
 def extract_end_date(file_content):
     try:
-        # Correct the regex pattern: Remove the extra parenthesis
-        enddate_match = re.search(r'(JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER)\s(\d{1,2},\s\d{4})', file_content)
-        enddate = enddate_match.group(2) if enddate_match else None
-        enddate = datetime.strptime(enddate, '%B %d, %Y') if enddate else None
-        return enddate
+        enddate_match = re.search(r'Applications must be received by [A-Za-z]+, ([A-Za-z]+ \d{1,2}, \d{4})', file_content)
+        enddate = enddate_match.group(1) if enddate_match else None
+        
+        return datetime.strptime(enddate, '%B %d, %Y') if enddate else None
     except Exception as e:
         raise ValueError(f'Error extracting enddate: {e}')
+
 
 def extract_selection(file_content):
     try:
         selection_match = re.findall(r'([A-Z][a-z]+)(\s\.\s)+', file_content)
-        selections = [z[0] for z in selection_match] if selection_match else None
-        return selections
+        return [z[0] for z in selection_match] if selection_match else None
     except Exception as e:
         raise ValueError(f'Error extracting selection: {str(e)}')
 
@@ -84,8 +79,7 @@ def extract_education_length(file_content):
     try:
         education_pattern = r'(One|Two|Three|Four|Five|Six|Seven|Eight|Nine|Ten|one|two|three|four|five|six|seven|eight|nine|ten)(\s|-)(years?)\s(college|university)'
         education_match = re.search(education_pattern, file_content)
-        education_length = education_match.group(1) if education_match else None
-        return education_length
+        return education_match.group(1) if education_match else None
     except Exception as e:
         raise ValueError(f'Error extracting education_length: {str(e)}')
 
@@ -93,16 +87,14 @@ def extract_experience_length(file_content):
     try:
         experience_pattern = r'(One|Two|Three|Four|Five|Six|Seven|Eight|Nine|Ten|one|two|three|four|five|six|seven|eight|nine|ten)\s(years?)\s(of\sfull(-s|\s)time)'
         experience_match = re.search(experience_pattern, file_content)
-        experience_length = experience_match.group(1) if experience_match else None
-        return experience_length
+        return experience_match.group(1) if experience_match else None
     except Exception as e:
         raise ValueError(f'Error extracting experience_length: {str(e)}')
 
 def extract_application_location(file_content):
     try:
         app_location_match = re.search(r'(Applications? will only be accepted on-?line)', file_content, re.IGNORECASE)
-        app_loc = 'Online' if app_location_match else 'Mail or In Person'
-        return app_loc
+        return 'Online' if app_location_match else 'Mail or In Person'
     except Exception as e:
         raise ValueError(f'Error extracting application_location: {str(e)}')
 
